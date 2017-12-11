95bc76f4ddd7a505285c95d59ae378e09ab92dc7
Kasun Vithanage <alankasun@gmail.com>
2017-12-11 06:52:31 +0530

    initial commit

---
+++ recognition-server/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import uuid
+
+app = Flask(__name__)
+CORS(app)
+
+data = [
+    {'name' : 'kasun', 'payload' : 'ok'}
+]
+
+@app.route("/vehicle", methods=['POST'])
+def hello():
+    if 'image' in request.files:
+        f = request.files['image']
+        ext = '.' + f.filename.split('.')[-1]
+        f.save('tmp/' + str(uuid.uuid4()) + ext)
+        # Add here image processing
+        # set payload
+        return jsonify({'error' : 'false', 'message' : 'No vehicle ID found. Please retry or enter manually !', 'payload' : '123456'})
+   
+    return jsonify({'error' : 'true', 'message' : 'No image found'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+   
+    # initialize recognizion package
+
