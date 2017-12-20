8feab790b43f1598899dcb914db7304fd14dcfb4
Kasun Vithanage <alankasun@gmail.com>
2017-12-20 22:41:16 +0530

    owners updated

--- web/database/migrations/2017_11_28_135949_create_owners_table.php
+++ web/database/migrations/2017_11_28_135949_create_owners_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateOwnersTable extends Migration
 {
@@ -15,15 +15,15 @@ class CreateOwnersTable extends Migration
     {
         Schema::create('owners', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nic', 20)->unique();
+            $table->string('nic', 60)->unique();
             $table->string('first_name');
             $table->string('last_name');
-            $table->datetime('birth_of_date');
-            $table->enum('sex',['male', 'female', 'other']);
-            $table->enum('title', ['Mr', 'Mrs', 'Rev', 'Miss']);
+            $table->date('date_of_birth');
+            $table->enum('gender',['male', 'female']);
+            $table->enum('title', ['mr', 'mrs', 'rev', 'miss']);
             $table->string('address','100');
-            $table->string('district',15);
-	        $table->string('province',15);
+            $table->string('district',60);
+	        $table->string('province',60);
             $table->timestamps();
         });
     }
