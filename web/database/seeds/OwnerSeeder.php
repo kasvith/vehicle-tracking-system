ccb9c7af9115fc9c2376fbf5488f2df289f1f16b
Kasun Vithanage <alankasun@gmail.com>
2017-12-20 22:41:03 +0530

    seeder updated for owners

---
+++ web/database/seeds/OwnerSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OwnerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    factory(App\Owner::class, 150)->create();
+    }
+}
